729c657a97c1d196be55b3c3e58362442d4f930a
Charlie Manaog <73146947+mncz@users.noreply.github.com>
2024-01-01 23:13:26 +0100

    Fix indentation space

--- java/Problem010.java
+++ java/Problem010.java
@@ -1,26 +1,26 @@
 // Summation of Primes
 
 public class Problem010 {
-	public static boolean is_prime(int n) {
-		if (n <= 1) return false;
-		
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) return false;
-		}
-		
-		return true;
-	}
+    public static boolean is_prime(int n) {
+        if (n <= 1)
+            return false;
 
-	public static void main(String[] args) {
-		long sum = 0;
-		
-		for (int i = 2; i < 2000000; i++) {
-			if (is_prime(i)) {
-				System.out.println(i);
-				sum += i;
-			};
-		}
-		
-		System.out.println(sum);
-	}
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        long sum = 0;
+
+        for (int i = 2; i < 2000000; i++) {
+            if (is_prime(i))
+                sum += i;
+        }
+
+        System.out.println(sum);
+    }
 }
