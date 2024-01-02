902a407d800ab68a94babaf99b9c92f3a02c6fec
Charlie Manaog <73146947+mncz@users.noreply.github.com>
2024-01-02 20:46:17 +0100

    Multiples of 3 or 5

---
+++ java/Problem001.java
@@ -0,0 +1,14 @@
+// Multiples of 3 or 5
+
+public class Problem001 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 3; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
