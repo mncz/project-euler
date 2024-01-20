b058da1edbcaae3948d9b570f526dabc71afdd96
Charlie Manaog <73146947+mncz@users.noreply.github.com>
2024-01-20 20:36:30 +0100

    `001.rs` aggiornato

--- rust/001.rs
+++ rust/001.rs
@@ -1,18 +1,11 @@
 // Multiples of 3 or 5
 
 fn main() {
-    let mut count = 0u32;
     let mut _sum = 0u32;
 
-    loop {
-        if count == 999 {
-            break;
-        }
-
-        count += 1;
-
-        if count % 3 == 0 || count % 5 == 0 {
-            _sum += count;
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            _sum += i;
         }
     }
 
